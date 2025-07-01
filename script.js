2b8513292a5958a1727a9db0562685c78db6ab3f
MarineMeyZielinski <m.zielinski.webdev@gmail.com>
2025-07-01 13:44:47 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Récupérer les éléments du DOM
+const displayEl = document.getElementById("display");
+const keysEl = document.getElementsByClassName("keys");
+const clearBtn = document.getElementsByClassName;
+
+//Initialisation du bouton clear
+let current = 0; // Current est le chiffre/nombre affiché sur l'écran de la calculatrice
+let previous = null; // propriété en lecture seule renvoie l'élément antérieur à celui spécifié dans childrenl a liste de son parent, ou null si l'élément spécifié est le premier de la liste.
+let op = 
+//
