4bb7564b0202d7bfcdece5b7419e3ec6d62168f7
MarineMeyZielinski <m.zielinski.webdev@gmail.com>
2025-07-01 15:01:47 +0200

    update script

--- script.js
+++ script.js
@@ -6,5 +6,10 @@ const clearBtn = document.getElementsByClassName;
 //Initialisation du bouton clear
 let current = 0; // Current est le chiffre/nombre affiché sur l'écran de la calculatrice
 let previous = null; // propriété en lecture seule renvoie l'élément antérieur à celui spécifié dans childrenl a liste de son parent, ou null si l'élément spécifié est le premier de la liste.
-let op = 
-//
+let op = null; // Opérateurs
+let resetNext = false; // Indiquer sur l'écran qu'au prochain clic ou numéro saisi, lcelui-ci doit se réinitialiser
+
+//Faire apparâitre current sur l'écran de la calculatrice
+function upateDisplay() {
+  displayEl.textContent = current;
+}
